df57619587832c8eedec24a07cd46842596bcedc
Myke Cameron <mykecameron@gmail.com>
2020-09-06 22:08:55 -0400

    WIP: sending texts one at a time

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_231258) do
+ActiveRecord::Schema.define(version: 2020_09_07_012336) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
@@ -27,6 +28,17 @@ ActiveRecord::Schema.define(version: 2020_09_04_231258) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "text_messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "from"
+    t.string "to"
+    t.string "body"
+    t.text "raw"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sent"
+    t.string "error"
   end
 
   create_table "users", force: :cascade do |t|
