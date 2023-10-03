e5e96704de5e3666a8dd0717f5bf580bf1780ccc
Myke Cameron <mykecameron@gmail.com>
2023-10-03 11:15:00 -0400

    jam twilio hellow world into view

--- hotline/views.py
+++ hotline/views.py
@@ -1,5 +1,6 @@
 import os
 from django.shortcuts import render
+from twilio.rest import Client as TwilioClient
 
 # Create your views here.
 
@@ -8,5 +9,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    SECRET = os.environ['SECRET']
-    return HttpResponse(f"Hello, world. You're at the hotline index. The secret is {SECRET}")
+    twilio_sid = os.environ['TWILIO_SID']
+    twilio_token = os.environ['TWILIO_AUTH_TOKEN']
+
+    client = TwilioClient(twilio_sid, twilio_token)
+
+    message = client.messages.create(
+        to="+17745730274",
+        from_="+18722667863",
+        body="Hello from the hotline app")
+
+    return HttpResponse(f"A message has been sent! SID: {message.sid}")
