01dd92cc89f0ba2ce93ce0fa7c84f2b8e1985ffd
Myke Cameron <mykecameron@gmail.com>
2023-10-12 13:07:35 -0400

    basic twilio voice webhook to answer

--- hotline/urls.py
+++ hotline/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("incoming", views.incoming, name="incoming"),
 ]
--- hotline/views.py
+++ hotline/views.py
@@ -1,17 +1,16 @@
 import os
 from django.shortcuts import render
 from twilio.rest import Client as TwilioClient
-
-# Create your views here.
-
+from twilio.twiml.voice_response import VoiceResponse
 
 from django.http import HttpResponse
 
-
 def index(request):
+    """Send a text"""
+
     twilio_sid = os.environ['TWILIO_SID']
     twilio_token = os.environ['TWILIO_AUTH_TOKEN']
-
+    
     client = TwilioClient(twilio_sid, twilio_token)
 
     message = client.messages.create(
@@ -20,3 +19,13 @@ def index(request):
         body="Hello from the hotline app")
 
     return HttpResponse(f"A message has been sent! SID: {message.sid}")
+
+def incoming(request):
+    """Get a voice call"""
+
+    resp = VoiceResponse()
+
+    # Read a message aloud to the caller
+    resp.say("Hello world!")
+
+    return HttpResponse(resp)
